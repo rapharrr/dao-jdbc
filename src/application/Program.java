7c3263d2189ee207c001705d40d0e9074f36fca6
Raphael B <rapha18023@gmail.com>
2023-06-14 16:19:37 -0300

    SellerDaoJDBC, SellerDaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,7 +14,8 @@ public class Program {
 	
 
 		Seller seller = new Seller(21,"Rapha", "rapha@gmail.com", new Date(), 3000.0,obj);
-		System.out.println(seller);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		System.out.println(sellerDao);
 	}
 
 }
